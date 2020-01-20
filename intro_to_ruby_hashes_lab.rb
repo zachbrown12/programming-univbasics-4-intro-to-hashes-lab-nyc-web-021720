cd0eec51649139fa2ff66b5159bfd158fbf11fb8
Zach Brown <zachmb22@gmail.com>
2020-01-20 22:29:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,10 +22,12 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
+  hash[key]
   # return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
+  hash[key] = 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
