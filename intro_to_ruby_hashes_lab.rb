b1f5e25b1d1222d38a93127b3736593b4391b82c
Zach Brown <zachmb22@gmail.com>
2020-01-20 22:31:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,10 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash[key]
+  if hash[key] != nil
+    hash[key] += 1
+  else hash[key] = 1
+end
   # return the correct value using the hash and key parameters
 end
 
